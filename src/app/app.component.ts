eaece7b501762555722da7bf06035447650d1b63
Mita-R <Mita.raboya@falcorp.co.za>
2022-01-11 09:40:33 +0200

    added content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chart';
+
+  graph3 = {
+    data: [
+     { x: [0, 1, 2, 3, 4, 5, 6],
+    y: [1, 9, 4, 7, 5, 2, 4],}
+      
+    ],
+    mode: 'markers',
+    marker: {
+        size: [20, 40, 25, 10, 60, 90, 30],
+    }
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,10 @@
 import { NgModule } from '@angular/core';
+
+import * as PlotlyJS from 'plotly.js-dist';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +16,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
